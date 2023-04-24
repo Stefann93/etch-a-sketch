c58b08d3b232910c787cd273c3f87f9de998d4ea
Steefaan04 <ilic.stefann4@gmail.com>
2023-04-24 20:50:34 +0200

    Finished making the grid, need to work on painting Need to add: Eraser, Colors, Rainbow colors, way to hold down LMB to paint. Need to make the "tools" div with all the buttons and sliders

--- java-script.js
+++ java-script.js
@@ -1,6 +1,22 @@
 const container = document.querySelector("#container");
-for (let i = 0; i < 16; i++) {
-    const drawingSquare = document.createElement('div');
-    drawingSquare.classList.add("child");
-    container.appendChild(drawingSquare);
+const gridSizeVal = document.querySelector("#gridSizeVal");
+const gridSize = document.querySelector("#gridSize");
+window.onload = createGrid();
+gridSize.addEventListener('input', createGrid);
+
+function createGrid() {
+    gridSizeVal.textContent = `Grid size: ${gridSize.value}  x  ${gridSize.value} `;
+    container.style.cssText = `grid-template-columns: repeat(${gridSize.value}, 1fr); grid-template-rows: repeat(${gridSize.value}, 1fr);`;
+    container.textContent = "";
+    for (let i = 0; i < Math.pow(gridSize.value, 2); i++) {
+        const drawingSquare = document.createElement('div');
+        drawingSquare.classList.add("child");
+        container.appendChild(drawingSquare);
+    }
 }
+const squares = document.querySelectorAll(".child")
+squares.forEach(square => {
+    square.addEventListener('mouseover', function(){
+        square.style.cssText = "background-color:black";
+    })
+});
