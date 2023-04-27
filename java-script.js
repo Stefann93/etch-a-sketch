a3f853216a9397b562fa3f243335d8871435c82f
Steefaan04 <ilic.stefann4@gmail.com>
2023-04-27 20:32:11 +0200

    Added Grid lines setting, and completed the drawing only if the LMB is held down what is left to do is: Colors, Eraser, Rainbow Colors, Clear and finish off the "tools" div with those three

--- java-script.js
+++ java-script.js
@@ -1,6 +1,9 @@
 const container = document.querySelector("#container");
 const gridSizeVal = document.querySelector("#gridSizeVal");
 const gridSize = document.querySelector("#gridSize");
+const gridLines = document.querySelector("#gridLines");
+let isPressed = false;
+let squares;
 window.onload = createGrid();
 gridSize.addEventListener('input', createGrid);
 
@@ -10,13 +13,48 @@ function createGrid() {
     container.textContent = "";
     for (let i = 0; i < Math.pow(gridSize.value, 2); i++) {
         const drawingSquare = document.createElement('div');
+        if (i == 0) {
+            drawingSquare.style.cssText = "border-top-left-radius: 15px;"
+            drawingSquare.setAttribute("id", "edge");
+        } else if (i == gridSize.value - 1) {
+            drawingSquare.style.cssText = "border-top-right-radius: 15px;"
+            drawingSquare.setAttribute("id", "edge");
+        }
+        else if (i == (gridSize.value - 1) * gridSize.value) {
+            drawingSquare.style.cssText = "border-bottom-left-radius: 15px;"
+            drawingSquare.setAttribute("id", "edge");
+        }
+        else if (i == Math.pow(gridSize.value, 2) - 1) {
+            drawingSquare.style.cssText = "border-bottom-right-radius: 15px;"
+            drawingSquare.setAttribute("id", "edge");
+        }
         drawingSquare.classList.add("child");
         container.appendChild(drawingSquare);
     }
+    squares = document.querySelectorAll(".child");
+    squares.forEach(square => {
+        square.addEventListener('mousedown', function () {
+            isPressed = true;
+        })
+        square.addEventListener('mouseup', function () {
+            isPressed = false;
+        })
+        square.addEventListener('mouseover', function () {
+            if (isPressed == true)
+                square.style.cssText = "background-color:black";
+        })
+    });
+    gridLines.checked = false;
 }
-const squares = document.querySelectorAll(".child")
-squares.forEach(square => {
-    square.addEventListener('mouseover', function(){
-        square.style.cssText = "background-color:black";
-    })
-});
+gridLines.addEventListener("change", function () {
+    if (this.checked) {
+        squares.forEach(square => {
+            square.classList.add("border");
+        });
+    }
+    else {
+        squares.forEach(square => {
+            square.classList.remove("border");
+        });
+    }
+})
